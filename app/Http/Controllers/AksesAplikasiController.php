daabad1fdfd88d9502997302fb75bc26f82aca9e
windi fikriyansyah <diwin321@gmail.com>
2026-07-23 18:53:59 +0700

    m

--- app/Http/Controllers/AksesAplikasiController.php
+++ app/Http/Controllers/AksesAplikasiController.php
@@ -777,7 +777,7 @@ PROMPT;
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
             ])->timeout(120)->post(
-                    "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=" . $apiKey,
+                    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" . $apiKey,
                     [
                         'contents' => [
                             [
